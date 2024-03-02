2376a1d8efa4f5f3e3b81ea641e21d38b318239b
Markowian22 <120415395+Markowian22@users.noreply.github.com>
2024-03-02 17:21:28 +0100

    comments to decorator

--- decorator.py
+++ decorator.py
@@ -3,6 +3,13 @@ from time import sleep
 
 
 def delay(seconds):
+    """
+    This is a decorator that introduces a delay before calling the decorated function.
+
+    :param seconds: The number of seconds to delay.
+    :type seconds: int
+    """
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -16,6 +23,13 @@ def delay(seconds):
 
 
 def special_key_decorator(func):
+    """
+    This is a decorator that handles special keys for the decorated function.
+
+    :param func: The function to decorate.
+    :type func: function
+    """
+
     @wraps(func)
     def wrapper(self, key):
         # print(func.__name__)
